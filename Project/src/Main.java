f7ee350c1de605b19097e1de98076ee7f66db212
Svirid_Polina <polina.svirid.03@mail.ru>
2022-07-14 13:57:32 +0300

    Agg task

---
+++ Project/src/Main.java
@@ -0,0 +1,14 @@
+class Base<T>{}
+class Derived<T> {}
+
+public class Main{
+public static void main(String[] args) {
+    Integer i1 = 5;
+    Integer i2 = 5;
+     if (i1.equals(i2)){
+         System.out.println("kjhg");
+     }
+}
+}
+
+
